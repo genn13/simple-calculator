ea14465c491b2c915acf71dd072319b47c369961
grigoryan-m <waldm4rk@gmail.com>
2026-04-19 19:34:40 +0400

    Initial commit: simple calculator

---
+++ main.py
@@ -0,0 +1,28 @@
+while True:
+    
+    try:
+        num1 = float(input('Enter first number: '))
+        num2 = float(input('Enter second number: '))
+
+        op = input('Enter an operation(+, -, *, /) :')
+
+        if op == '+':
+            print(f'{num1} + {num2} = {num1+num2}')
+        elif op == '-':
+            print(f'{num1} - {num2} = {num1-num2}')
+        elif op == '*':
+            print(f'{num1} * {num2} = {num1*num2}')
+        elif op == '/':
+            if num2==0:
+                print("You can't divide by zero")
+            else:
+                print(f'{num1} / {num2} = {num1/num2}')
+        else:
+            print('Unknown operation')
+        choice = input('Do you want to continue?(Y/N)').lower()
+        if choice == 'n':
+            break
+        
+    except ValueError:
+        print('Please enter valid numbers')
+    
